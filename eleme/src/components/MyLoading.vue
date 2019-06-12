75b624199c8ffb80d586e36163f79b1b15d8f781
HansonGor <49916467+HansonGor@users.noreply.github.com>
2019-06-13 00:58:20 +0800

    elema

---
+++ eleme/src/components/MyLoading.vue
@@ -0,0 +1,25 @@
+<style>
+@import "../css/weui.css";
+</style>
+<template>
+    <div id="loadingToast" :style="{
+    opacity: loading?1:0, 
+     display: loading?'block':'none'
+     }">
+        <div class="weui-mask_transparent"></div>
+        <div class="weui-toast">
+            <i class="weui-loading weui-icon_toast"></i>
+            <p class="weui-toast__content">数据加载中</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  computed: {
+    loading() {
+      // console.log(this);
+      return this.$store.state.loading;
+    }
+  }
+};
+</script>
